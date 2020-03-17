101b257db5dca2cca139e3b54bc85badb05ba797
yoshiki <yoshiki0885@gmail.com>
2020-03-17 16:36:50 +0900

    first commit

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -9,12 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        //Randomize some number
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        //Update image View
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+       
+        
+        
+         //Compare the random Numbers
+        if leftScoreLabel.text == String("WIN!!"){
+            leftScoreLabel.text = String(leftScore)
+            rightScoreLabel.text = String(rightScore)
+        }else if rightScoreLabel.text == String("WIN!!"){
+            leftScoreLabel.text = String(leftScore)
+            rightScoreLabel.text = String(rightScore)
+        }
+        
+        if leftNumber > rightNumber{
+            //leftside wins
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber{
+            //rightside wins
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        else{
+            //tie
+        }
+        
+        if leftScore == 7{
+            leftScoreLabel.text = String("WIN!!")
+            leftScore = 0
+            rightScore = 0
+        }else if rightScore == 7{
+            rightScoreLabel.text = String("WIN!!")
+            leftScore = 0
+            rightScore = 0
+        }
+    }
+    
 }
 
